bbc0aa699eea5e0ce2f65e59a426d4637d42c597
Moulouk <79087739+Moulouk321@users.noreply.github.com>
2024-05-03 21:09:32 +0300

    Users Panel

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/user.router.js");
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
+    const __dirname = path.resolve();
+
+const app = express();
+app.use(cookieParser());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/', userRouter);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
